cf6a4c90694b11d474ef4d80e6c0258915aeb9a7
Martins Sipenko <martins.sipenko@gmail.com>
2012-08-17 11:59:34 +0300

    Added database schema

---
+++ schema/schema.sql
@@ -0,0 +1,10 @@
+--
+-- Table structure for table `apgrieziens`
+--
+
+CREATE TABLE IF NOT EXISTS `apgrieziens` (
+  `num` int(4) NOT NULL DEFAULT '0',
+  `sta` int(11) DEFAULT NULL,
+  `apg` int(11) DEFAULT NULL,
+  PRIMARY KEY (`num`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
